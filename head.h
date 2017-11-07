6f4eb57dd6da1647deea7e78aad77805b6180988
KLzfyn520 <32433033+KLzfyn520@users.noreply.github.com>
2017-11-07 20:15:31 +0800

    Create head.h

---
+++ head.h
@@ -0,0 +1,110 @@
+#ifdef HEAD_H
+#define HEAD_H
+#endif // HEAD_H
+
+
+#include <stdio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+void Assert(bool val, string s) {
+	if (!val) {
+		cout << "Assertion Failed:" << s << endl;
+		exit(-1);
+	}
+}
+template<typename E>class Stack {
+private:
+	void operator = (const Stack&) {}
+	Stack(const Stack&) {}
+public:
+	Stack() {}
+	virtual ~Stack() {}
+
+	virtual void clear() = 0;
+
+	virtual void push(const E& it) = 0;
+
+	virtual E pop() = 0;
+
+	virtual const E& topValue() const = 0;
+
+	virtual int length() const = 0;
+};
+
+template<typename E>class Link {
+private:
+	static Link<E>* freelist;
+public:
+	E element;
+	Link* next;
+
+	Link(const E& elemval, Link* nextval = NULL) {
+		element = elemval; next = nextval;
+	}
+
+	Link(Link* nextval = NULL) {
+		next = nextval;
+	}
+
+	void* operator new(size_t) {
+		if (freelist == NULL) return ::new Link;
+		Link<E>* temp = freelist;
+		freelist = freelist->next;
+		return temp;
+	}
+
+	void operator delete(void* ptr) {
+		((Link<E>*)ptr)->next = freelist;
+		freelist = (Link<E>*)ptr;
+	}
+};
+
+template<typename E>
+Link<E>* Link<E>::freelist = NULL;
+
+template<typename E>class LStack : public Stack<E> {
+private:
+	Link<E>* top;
+	int size;
+public:
+	LStack(int sz = 50) {
+		top = NULL;
+		size = 0;
+	}
+
+	~LStack() { clear(); }
+
+	void clear() {
+		while (top != NULL) {
+			Link<E>* temp = top;
+			top = top->next;
+			delete temp;
+		}
+		size = 0;
+	}
+
+	void push(const E&it) {
+		top = new Link<E>(it, top);
+		size++;
+	}
+
+	E pop() {
+		Assert(top != NULL, "Stack is empty");
+		E it = top->element;
+		Link<E>* ltemp = top->next;
+		delete top;
+		top = ltemp;
+		size--;
+		return it;
+	}
+
+	const E& topValue() const {
+		Assert(top != NULL, "Stack is empty");
+		 return top->element;
+	}
+
+	int length() const { return size; }
+};
+#pragma once
